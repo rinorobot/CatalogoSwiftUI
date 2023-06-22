12b076b0b1b3cc3398c46fb770fac59076dbc8de
rinorobot <sgomez_moya@hotmail.com>
2023-06-22 06:52:47 -0600

    Initial Commit

---
+++ CatalogoSwiftUI/CatalogoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatalogoSwiftUIApp.swift
+//  CatalogoSwiftUI
+//
+//  Created by Salvador Gómez Moya on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CatalogoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
